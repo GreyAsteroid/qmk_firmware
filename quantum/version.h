54c5f7613bc4b43f6bad2f8aa862f1fea55dd4dc
Eric Karnes <eric@karnes.me>
2020-07-21 18:14:35 -0400

    Modify keymap

--- quantum/version.h
+++ quantum/version.h
@@ -1,4 +1,4 @@
-#define QMK_VERSION "0.9.43-22-g3b07f4-dirty"
-#define QMK_BUILDDATE "2020-07-21-00:15:07"
-#define CHIBIOS_VERSION "0.9.43-22-g3b07f4-dirty"
-#define CHIBIOS_CONTRIB_VERSION "0.9.43-22-g3b07f4-dirty"
+#define QMK_VERSION "0.9.43-28-g2acaec-dirty"
+#define QMK_BUILDDATE "2020-07-21-18:10:20"
+#define CHIBIOS_VERSION "0.9.43-28-g2acaec-dirty"
+#define CHIBIOS_CONTRIB_VERSION "0.9.43-28-g2acaec-dirty"
